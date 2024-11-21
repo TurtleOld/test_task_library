708a09bcaf7053f55c4a6daf45722cb18bc6f8ab
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-21 17:22:35 +0300

    refact: change pytest test to unittest

--- tests/test_library.py
+++ tests/test_library.py
@@ -1,64 +1,57 @@
 import os.path
-from typing import Any, Generator
-
-import pytest
+import unittest
 
 from library.engine.library import Library
 
 
-@pytest.fixture()
-def library() -> Generator[Library, Any, None]:
-    filename = "library.json"
-    lib = Library(filename=filename)
-    yield lib
-    if os.path.exists(filename):
-        os.remove(filename)
+class TestLibrary(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.filename = "library.json"
+        self.library = Library(filename=self.filename)
 
-def test_add_book(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    assert len(library.books) == 1
-    assert library.books[0]["title"] == "Новая книга"
-    assert library.books[0]["author"] == "Иван Иванов"
-    assert library.books[0]["year"] == 2024
-    assert library.books[0]["status"] == "в наличии"
+    def tearDown(self) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
 
+    def test_add_book(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.assertEqual(len(self.library.books), 1)
+        self.assertEqual(self.library.books[0]["title"], "Новая книга")
+        self.assertEqual(self.library.books[0]["author"], "Иван Иванов")
+        self.assertEqual(self.library.books[0]["year"], 2024)
+        self.assertEqual(self.library.books[0]["status"], "в наличии")
 
-def test_remove_book(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    library.remove_book(book_id=1)
-    assert len(library.books) == 0
+    def test_remove_book(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.remove_book(book_id=1)
+        self.assertEqual(len(self.library.books), 0)
 
+    def test_display_book(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.assertEqual(self.library.books[0], {
+            "author": "Иван Иванов",
+            "id": 1,
+            "status": "в наличии",
+            "title": "Новая книга",
+            "year": 2024,
+        })
 
-def test_display_book(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    assert library.books[0] == {
-        "author": "Иван Иванов",
-        "id": 1,
-        "status": "в наличии",
-        "title": "Новая книга",
-        "year": 2024,
-    }
+    def test_search_book(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.assertTrue(self.library.search_books("Новая книга"))
 
+    def test_change_status(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.change_status(1, "выдана")
+        self.assertEqual(self.library.books[0]["status"], "выдана")
 
-def test_search_book(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    assert library.search_books("Новая книга")
+    def test_change_status_invalid(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.change_status(1, "неизвестный статус")
+        self.assertEqual(self.library.books[0]["status"], "в наличии")
 
-
-def test_change_status(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    library.change_status(1, "выдана")
-    assert library.books[0]["status"] == "выдана"
-
-
-def test_change_status_invalid(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    library.change_status(1, "неизвестный статус")
-    assert library.books[0]["status"] == "в наличии"
-
-
-def test_remove_nonexistent_book(library: Library) -> None:
-    library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-    library.remove_book(999)
-    assert len(library.books) == 1
+    def test_remove_nonexistent_book(self) -> None:
+        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.remove_book(999)
+        self.assertEqual(len(self.library.books), 1)
