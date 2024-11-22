318f84a9ae283c028b79fcab669e5332450717f0
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-22 10:24:10 +0300

    chore: add docstrings + ruff format

--- library/engine/library.py
+++ library/engine/library.py
@@ -4,18 +4,29 @@ from typing import List, Dict, Any
 
 class Library:
     def __init__(self, filename: str = "library.json") -> None:
+        """
+        Инициализирует библиотеку с указанным именем файла для хранения книг.
+
+        Аргументы:
+            filename (str): Имя файла для загрузки/сохранения данных библиотеки.
+            По умолчанию "library.json".
+        """
         self.filename = filename
         self.books: List[Dict[str, Any]] = []
         self.load_books()
 
     def load_books(self) -> None:
+        """
+        Загружает книги из указанного JSON-файла в библиотеку.
+
+        Если файл не найден или содержит недопустимый JSON,
+        библиотека начинается с пустого списка книг.
+        """
         try:
             with open(self.filename, "r") as file:
                 self.books = json.load(file)
         except FileNotFoundError:
-            print(
-                f"Файл {self.filename} не найден. Начинаем с пустой библиотеки."
-            )
+            print(f"Файл {self.filename} не найден. Начинаем с пустой библиотеки.")
         except json.JSONDecodeError:
             print(
                 f"Ошибка декодирования JSON из файла {self.filename}. Начинаем с пустой библиотеки."
@@ -24,10 +35,19 @@ class Library:
             print(f"Произошла непредвиденная ошибка: {e}")
 
     def save_books(self) -> None:
+        """Сохраняет текущий список книг в указанный JSON-файл."""
         with open(self.filename, "w", encoding="utf-8") as file:
             json.dump(self.books, file, ensure_ascii=False, indent=4)
 
     def add_book(self, title: str, author: str, year: int) -> None:
+        """
+        Добавляет новую книгу в библиотеку.
+
+        Аргументы:
+            title (str): Название книги.
+            author (str): Автор книги.
+            year (int): Год издания книги.
+        """
         book_id = len(self.books) + 1
         book = {
             "id": book_id,
@@ -41,6 +61,12 @@ class Library:
         print(f"Книга {title} сохранена с id {book_id}")
 
     def remove_book(self, book_id: int) -> None:
+        """
+        Удаляет книгу из библиотеки по ее ID.
+
+        Аргументы:
+            book_id (int): ID книги, которую нужно удалить.
+        """
         for book in self.books:
             if book["id"] == book_id:
                 self.books.remove(book)
@@ -50,6 +76,15 @@ class Library:
             print(f"Книга с id {book_id} не найдена")
 
     def search_books(self, query: str) -> List[Dict[str, Any]]:
+        """
+        Ищет книги по названию, автору или году издания.
+
+        Аргументы:
+            query (str): Запрос для поиска в названиях книг, именах авторов или годах.
+
+        Возвращает:
+            List[Dict[str, Any]]: Список книг, соответствующих критериям поиска.
+        """
         results = [
             book
             for book in self.books
@@ -60,6 +95,10 @@ class Library:
         return results
 
     def display_books(self) -> None:
+        """
+        Отображает все книги в библиотеке.
+        Если библиотека пуста, выводит соответствующее сообщение.
+        """
         if not self.books:
             print("Библиотека пуста.")
             return
@@ -71,6 +110,13 @@ class Library:
             )
 
     def change_status(self, book_id: int, new_status: str) -> None:
+        """
+        Изменяет статус книги по ее ID.
+
+        Аргументы:
+            book_id (int): ID книги, статус которой нужно изменить.
+            new_status (str): Новый статус книги. Должен быть либо "в наличии", либо "выдана".
+        """
         if new_status not in ["в наличии", "выдана"]:
             print('Неверный статус. Доступные статусы: "в наличии", "выдана".')
             return
--- tests/test_library.py
+++ tests/test_library.py
@@ -5,7 +5,6 @@ from library.engine.library import Library
 
 
 class TestLibrary(unittest.TestCase):
-
     def setUp(self) -> None:
         self.filename = "library.json"
         self.library = Library(filename=self.filename)
@@ -15,7 +14,11 @@ class TestLibrary(unittest.TestCase):
             os.remove(self.filename)
 
     def test_add_book(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.assertEqual(len(self.library.books), 1)
         self.assertEqual(self.library.books[0]["title"], "Новая книга")
         self.assertEqual(self.library.books[0]["author"], "Иван Иванов")
@@ -23,35 +26,62 @@ class TestLibrary(unittest.TestCase):
         self.assertEqual(self.library.books[0]["status"], "в наличии")
 
     def test_remove_book(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.library.remove_book(book_id=1)
         self.assertEqual(len(self.library.books), 0)
 
     def test_display_book(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
-        self.assertEqual(self.library.books[0], {
-            "author": "Иван Иванов",
-            "id": 1,
-            "status": "в наличии",
-            "title": "Новая книга",
-            "year": 2024,
-        })
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
+        self.assertEqual(
+            self.library.books[0],
+            {
+                "author": "Иван Иванов",
+                "id": 1,
+                "status": "в наличии",
+                "title": "Новая книга",
+                "year": 2024,
+            },
+        )
 
     def test_search_book(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.assertTrue(self.library.search_books("Новая книга"))
 
     def test_change_status(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.library.change_status(1, "выдана")
         self.assertEqual(self.library.books[0]["status"], "выдана")
 
     def test_change_status_invalid(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.library.change_status(1, "неизвестный статус")
         self.assertEqual(self.library.books[0]["status"], "в наличии")
 
     def test_remove_nonexistent_book(self) -> None:
-        self.library.add_book(title="Новая книга", author="Иван Иванов", year=2024)
+        self.library.add_book(
+            title="Новая книга",
+            author="Иван Иванов",
+            year=2024,
+        )
         self.library.remove_book(999)
         self.assertEqual(len(self.library.books), 1)
